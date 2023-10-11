41731b17aef376ed868f4274def3cd2b9d5b27c1
madhurimunakala <146205731+madhurimunakala@users.noreply.github.com>
2023-10-11 21:13:17 +0530

    Add files via upload

---
+++ src/javaprograms/FirstThread.java
@@ -0,0 +1,27 @@
+package javaprograms;
+
+public class FirstThread extends Thread {
+   public void run(){
+	   System.out.println("this is a run method ");
+   }
+   public FirstThread() {
+	   System.out.println("firstthread constructor");
+   }
+   public static void main(String a[]) {
+	   FirstThread ft1=new FirstThread();
+	   FirstThread ft2=new FirstThread();
+	   //ft1.run();
+	   ft1.start();
+	   //ft2.start();
+	   System.out.println("Default thread priority of Thread 1: " + ft1.getPriority());
+	   System.out.println("Default thread priority of Thread 2: " + ft2.getPriority());
+	   ft1.setPriority(MAX_PRIORITY);
+	   ft2.setPriority(MIN_PRIORITY);
+	   System.out.println("Default thread priority of Thread 1: " + ft1.getPriority());
+	   System.out.println("Default thread priority of Thread 2: " + ft2.getPriority());
+	  /* System.out.println("\n" + Thread.currentThread().getName());
+	      System.out.println("Default thread priority of Main Thread: " + Thread.currentThread().getPriority());
+	      Thread.currentThread().setPriority(MAX_PRIORITY);
+	      System.out.println("The maximum thread priority of Main Thread is: " + Thread.currentThread().getPriority());*/
+   }
+}
---
+++ src/javaprograms/ThreadExRunnableInterface.java
@@ -0,0 +1,23 @@
+package javaprograms;
+
+public class ThreadExRunnableInterface implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("this is example for runnable interface");
+	}
+	
+public static void main(String a[]) {
+	ThreadExRunnableInterface ft2=new ThreadExRunnableInterface();
+	//ft2.start();
+   Thread t1=new Thread(ft2);
+   Thread t2=new Thread(ft2,"ThreadExRunnableInterface");
+   t1.start();
+   t2.start();
+   System.out.println("Default thread priority of Thread 1: " + t1.getPriority());
+   System.out.println("Default thread priority of Thread 2: " + t2.getPriority());
+   
+   
+}
+}
