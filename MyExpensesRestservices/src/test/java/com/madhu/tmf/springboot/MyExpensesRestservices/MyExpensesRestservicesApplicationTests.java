e471e6027657bddaec519e736cda425190df83e1
madhurimunakala <146205731+madhurimunakala@users.noreply.github.com>
2023-11-27 22:11:20 +0530

    added new spring boot application

---
+++ MyExpensesRestservices/src/test/java/com/madhu/tmf/springboot/MyExpensesRestservices/MyExpensesRestservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhu.tmf.springboot.MyExpensesRestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExpensesRestservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
