cda676cc3ca07450c0e78ce54daf2288b95f4016
munakalamadhuri <munakalamadhuri405@gmail.com>
2023-11-29 18:55:05 +0530

    update the files

---
+++ src/main/java/com/madhu/tmf/springboot/MyExpensesRestservices/MyExpensesRestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.madhu.tmf.springboot.MyExpensesRestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpensesRestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExpensesRestservicesApplication.class, args);
+	}
+
+}
